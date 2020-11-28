e148c4744521b8e7680f4a8d26b3ee6e58a3ab08
RonaldKiprotich <protich12@gmail.com>
2020-11-28 15:27:36 +0300

    add user authentication

--- awwards/urls.py
+++ awwards/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
+from . import views
 
 urlpatterns = [
-   
+    
+    path('accounts/register/', views.registration, name='register'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 ]
