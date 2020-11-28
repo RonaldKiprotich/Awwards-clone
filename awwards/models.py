10b0749484571ea0750b0901d3fe5743af1c746b
RonaldKiprotich <protich12@gmail.com>
2020-11-28 16:42:31 +0300

    add urls for profile

--- awwards/models.py
+++ awwards/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from django.db.models.signals import post_save
 from django.dispatch  import receiver
-from django.http import 
+from django.http import Http404
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
