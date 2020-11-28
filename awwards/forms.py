5c409db8b636ae22b4401d3d0aeef6f1301ebfa2
RonaldKiprotich <protich12@gmail.com>
2020-11-28 18:09:56 +0300

    add user update form

--- awwards/forms.py
+++ awwards/forms.py
@@ -10,3 +10,10 @@ class RegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['email']
+
--- awwards/views.py
+++ awwards/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .forms import *
+from .models import *
 
 # Create your views here.
 def registration(request):
