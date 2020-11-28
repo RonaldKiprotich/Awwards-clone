df4e8042902d05ba8b091b1d3b80029da582b6e5
RonaldKiprotich <protich12@gmail.com>
2020-11-29 00:18:08 +0300

    add urls for update profile

--- awwards/urls.py
+++ awwards/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('accounts/profile/', views.profile, name='profile'),
+    path('updateprofile/', views.updateprofile, name='updateprofile'),
 ]
