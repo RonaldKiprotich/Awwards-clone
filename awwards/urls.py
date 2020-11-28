b18af99e582410984def09eda7b57a262eeccf7f
RonaldKiprotich <protich12@gmail.com>
2020-11-28 17:09:43 +0300

    add views method for profile

--- awwards/urls.py
+++ awwards/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -10,5 +11,3 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('accounts/profile/', views.profile, name='profile'),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- awwards/views.py
+++ awwards/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
 from .forms import *
 
 # Create your views here.
@@ -15,3 +16,27 @@ def registration(request):
         'form':form,
     }
     return render(request, 'registration/register.html', context)
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    projects = Projects.objects.all()
+    posts = Profile.objects.all()
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Your account has been successfully updated')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+    'user_form':user_form,
+    'profile_form':profile_form,
+    'posts':posts,
+    'projects':projects,
+    }
+    return render(request, 'profile/profile.html', context)
