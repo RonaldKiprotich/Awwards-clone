e7c48653e8bd495c679a94d71ca525d78ea5bcb9
RonaldKiprotich <protich12@gmail.com>
2020-11-29 00:21:01 +0300

    add profile update form

--- awwards/forms.py
+++ awwards/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import *
 
 
 class RegisterForm(UserCreationForm):
@@ -16,4 +17,10 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image', 'bio']
+
 
