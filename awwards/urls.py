1b706537ef9c05276a0b2635d570b4ee7359d1f4
RonaldKiprotich <protich12@gmail.com>
2020-11-30 07:58:34 +0300

    debug so as to read static files

--- awwards/urls.py
+++ awwards/urls.py
@@ -19,3 +19,6 @@ urlpatterns = [
     path('api/profile/', views.ProfileList.as_view()),
     path('api/projects/', views.ProjectsList.as_view()),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- awwardsclone/settings.py
+++ awwardsclone/settings.py
@@ -135,9 +135,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STAR_RATINGS_RANGE=10
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = '/' 
-
 
 
 cloudinary.config( 
